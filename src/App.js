9f126534fdc809c02b0c6ed831c30cbd1ed989fa
hikarinakano <hikarinakano95@gmail.com>
2024-09-24 11:30:34 +0200

    Refactored code for the main app to pass new functions as props and to understand better how this system works

--- src/App.js
+++ src/App.js
@@ -1,13 +1,39 @@
 import { useState } from "react";
 import BookCreate from './components/BookCreate';
+import BookList from "./components/BookList";
 
 function App () {
     const [books, setBooks] = useState([]);
+
     const handleCreateBook = (title) => {
-        console.log('need to add book with:', title)
+        const id = Math.round(Math.random() * 9999);
+        const updatedBooks = [
+            ...books,
+            { id, title },
+        ]
+        setBooks(updatedBooks);
     }
+    
+    const deleteBookById = (id) => {
+        const updatedBooks = books.filter((book) => {
+            return book.id !== id;
+        })
+        setBooks(updatedBooks);
+    }
+
+    const editBookById = (id, newTitle) => {
+        const updatedBooks = books.map((book) => {
+            if (book.id === id) {
+                return {...book, title: newTitle};
+            }
+            return book;
+        })
+        setBooks(updatedBooks);
+    }
+
     return (
-        <div>
+        <div className="app">
+            <BookList books={books} onDelete={deleteBookById} onEdit={editBookById}/>
             <BookCreate onCreate={handleCreateBook}/>
         </div>
     );
