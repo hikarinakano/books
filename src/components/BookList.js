0acd9063ffe26197ea77afffe7d895aa2572ae0e
hikarinakano <hikarinakano95@gmail.com>
2024-09-24 11:29:52 +0200

    added components to create a book, to render book list to show books and to edit books using form

--- src/components/BookList.js
+++ src/components/BookList.js
@@ -1,5 +1,11 @@
-function BookList () {
-    return <div>BookList</div>;
+import BookShow from './BookShow';
+
+function BookList ({ books, onDelete, onEdit }) {
+
+    const renderedBooks = books.map((book) => {
+        return <BookShow onDelete={onDelete} key={book.id} book={book} onEdit={onEdit} />
+    })
+    return <div>{renderedBooks}</div>;
 }
 
 export default BookList;
--- src/components/BookShow.js
+++ src/components/BookShow.js
@@ -1,9 +1,38 @@
 import { useState } from "react";
+import BookEdit from "./BookEdit";
 
-function BookShow () {
-    const [books, setBooks] = useState([]);
-    
-    return <div>BookShow</div>;
+function BookShow ({ book, onDelete, onEdit }) {
+    const [showEdit, setShowEdit] = useState(false);
+
+    const handleDeleteClick = () => {
+        onDelete(book.id);
+    }
+
+    const handleEditClick = () => {
+        setShowEdit(!showEdit);
+    }
+
+    const handleSubmit = () => {
+        setShowEdit(false);
+    }
+    let content = <h3>{book.title}</h3>;
+
+    if (showEdit) {
+        content = <BookEdit onSubmit={handleSubmit} book={book} onEdit={onEdit}/>;
+    }
+
+    return(
+        <div className="book-show">
+            <div>{content}</div>
+            <div className="actions">
+                <button className="edit" onClick={handleEditClick}>
+                    Edit
+                </button>
+                <button className="delete" onClick={handleDeleteClick}>
+                    Delete
+                </button>
+            </div>
+        </div>);
 }
 
 export default BookShow;
